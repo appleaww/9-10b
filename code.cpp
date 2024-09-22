9827b70fc09fce50400eef27a6c1af7a0de0b646
appleaww <artemmorozz2006@gmail.com>
2024-09-22 17:50:50 +0300

    Update code.cpp

---
+++ code.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a;
+int c;
+std::cout << "Введите число a\n";
+std::cin >>a;
+std::cout << "Введите число c\n";
+std::cin >>c;
+std::cout << "Сумма чисел равна " << a+c;
+return 0;
+}
